09822504fb8140b708c933d56052038cf6565ca7
Karthik <karthik1997.iyer@gmail.com>
2021-07-04 18:02:48 +0530

    'blog'

---
+++ src/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+    return(
+        <>
+        <footer className="bg-light text-center">
+        <p>© 2021 Karthik Iyer. All Rights Reserved |Terms and Condtions</p>
+
+        </footer>
+        </>
+
+    )
+}
+
+export default Footer;
---
+++ src/Sdata.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import webdev from './images/img2.jpg'
+import andriod from './images/img3.jpg'
+import software from './images/img4.jpg'
+import data from './images/img5.jpg'
+import digital from './images/img6.jpg'
+import cyber from './images/img7.jpg'
+
+const Sdata = [
+    {
+        imgsrc: webdev,
+        title: "Web Development",
+    },
+
+    {
+        imgsrc: andriod,
+        title: "App Development",
+    },
+
+    {
+        imgsrc: software,
+        title: "Software Development",
+    },
+
+    {
+        imgsrc: data,
+        title: "Data Scientist",
+    },
+
+    {
+        imgsrc: digital,
+        title: "Digital Marketing",
+    },
+
+    {
+        imgsrc: cyber,
+        title: "Cyber Security",
+    },
+
+    
+]
+
+export default Sdata;
